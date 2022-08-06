a8b29e578de69585864bef49102acb0c30325852
Nikita <nikitakireev91@gmail.com>
2022-08-06 14:57:03 +0300

    Firts commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// -alert('Hello');
+
+//const result = confirm('are u here?');
+
+//console.log(result);
+
+// const answer = + prompt('are you 18?' , '18+');
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// // answers[0] = prompt('Your name?','');
+// // answers[1] = prompt('How is ur age','');
+// // answers[2] = prompt('Your country?', '');
+
+// // document.write(answers);
+
+// console.log(typeof(answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const name = 'Nik';
+
+alert(`hi, ${name}`);
